d1b2d16fd2f5ca8abf5cb9e5f60a23c9fb2e2f6a
Djuanzz <115209488+Djuanzz@users.noreply.github.com>
2022-10-20 20:27:56 +0700

    Add files via upload

---
+++ KonversiBiner.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void biner(int n){
+    char konversi;
+    
+    if(n == 0){
+        return;
+    }
+
+    if(n % 2 == 0){
+        konversi = '0';
+    } 
+    
+    if(n % 2 != 0){
+        konversi = '1';
+    } 
+
+    biner(n / 2);
+    printf("%c", konversi);
+}
+
+int main(){
+    unsigned long long angka1, angka2, A, B, C;
+
+    scanf("%d", &angka1);
+    printf("Biner dari %d = ", angka1);
+    biner(angka1);
+
+    printf("\n");
+
+
+    return 0;
+}
